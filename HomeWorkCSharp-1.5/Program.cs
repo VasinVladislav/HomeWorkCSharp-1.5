31ffc5498836fdc4c3f38af2cd4d8666ec9b1ff8
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 12:16:44 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.5. Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними.
+            Console.WriteLine("1" + "13" + " 49");
+        }
+    }
+}
